6503c30e79cca2c6f4ccf5f24f9ee75125c2cca8
VictorMCruzB <victor.cruz17@tectijuana.edu.mx>
2018-09-28 02:41:56 -0700

    Unidad 2

---
+++ E2-1Factorial/E2-1Factorial/Métodos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_1Factorial
+{
+    class Métodos
+    {
+        public int CalcularFactorial(int n = 6)
+        {
+            int Factorial = 1;
+            for (int contador = n; contador >= 1; contador--)
+            {
+                Factorial = Factorial * contador;
+            }
+            return Factorial;
+        }
+
+        public int CalcularFactorialRecursividad(int n = 6)
+        {
+            if (n == 0)
+                return 1;
+            return n * CalcularFactorialRecursividad(n - 1);
+        }
+    }
+}
