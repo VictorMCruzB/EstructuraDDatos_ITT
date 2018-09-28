2aed7fda983f550607f1b7524b238a35c8d142e2
VictorMCruzB <victor.cruz17@tectijuana.edu.mx>
2018-09-28 08:02:40 -0700

    Unidad 2

---
+++ E2-1 CruzBetancourtVictorManuel/E2-1 CruzBetancourtVictorManuel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_1_CruzBetancourtVictorManuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Métodos Factorial = new Métodos();
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            TimeSpan parar;
+            TimeSpan iniciar = new TimeSpan(DateTime.Now.Ticks);
+            parar = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("Ciclo for\nFactorial #6 = {0}\nTiempo de Ejecución: {1} Segundos\n\nRecursividad\nFactorial #6 = {2}\nTiempo de ejecución: {3} Segundos", Factorial.CalcularFactorial(), stop.Subtract(start).TotalSeconds, Factorial.CalcularFactorialRecursividad(), parar.Subtract(iniciar).TotalSeconds);
+            Console.ReadKey();
+        }
+    }
+}
