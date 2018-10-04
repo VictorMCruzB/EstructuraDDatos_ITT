6bd266d69e430202276d418ece900ad990eb0e78
VictorMCruzB <victor.cruz17@tectijuana.edu.mx>
2018-10-04 05:32:48 -0700

    Prácticas Evaluativas U2

---
+++ PE-2.Cruz Betancourt Victor Manuel/PE-2.Cruz Betancourt Victor Manuel/Métodos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_2.Cruz_Betancourt_Victor_Manuel
+{
+    class Métodos
+    {
+        public int MínimoSucesión(int[] Sucesión, int Índice, int Mínimo)
+        {
+            if (Índice != Sucesión.Length)
+            {
+                if (Sucesión[Índice] < Mínimo)
+                {
+                    Mínimo = MínimoSucesión(Sucesión, Índice + 1, Sucesión[Índice]);
+                }
+                else
+                {
+                    Mínimo = MínimoSucesión(Sucesión, Índice + 1, Mínimo);
+                }
+            }
+            return Mínimo;
+        }
+        public int MáximoSucesión(int[] Sucesión, int Índice, int Máximo)
+        {
+            if (Índice != Sucesión.Length)
+            {
+                if (Sucesión[Índice] > Máximo)
+                {
+                    Máximo = MáximoSucesión(Sucesión, Índice + 1, Sucesión[Índice]);
+                }
+                else
+                {
+                    Máximo = MáximoSucesión(Sucesión, Índice + 1, Máximo);
+                }
+            }
+            return Máximo;
+        }
+        public void SucesiónInvertida(int[] Sucesión, int Principio, int Fin)
+        {
+            if (Principio < Fin)
+            {
+                int Auxiliar = Sucesión[Principio];
+                Sucesión[Principio] = Sucesión[Fin];
+                Sucesión[Fin] = Auxiliar;
+                SucesiónInvertida(Sucesión, Principio + 1, Fin - 1);
+            }
+        }
+    }
+}
---
+++ PE-2.Cruz Betancourt Victor Manuel/PE-2.Cruz Betancourt Victor Manuel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_2.Cruz_Betancourt_Victor_Manuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opc;
+            do
+            {
+                Console.WriteLine("¿Cúantos números tiene su sucesión?");
+                int n = int.Parse(Console.ReadLine());
+                int[] Sucesión = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Valor número {0} de la sucesión", i + 1);
+                    Sucesión[i] = int.Parse(Console.ReadLine());
+                }
+                Métodos Objeto = new Métodos();
+                Console.WriteLine("El valor mínimo de la sucesion es {0}", Objeto.MínimoSucesión(Sucesión, 0, Sucesión[0]));
+                Console.WriteLine("El valor máximo de la sucesion es {0}", Objeto.MáximoSucesión(Sucesión, 0, Sucesión[0]));
+                Objeto.SucesiónInvertida(Sucesión, 0, Sucesión.Length - 1);
+                Console.WriteLine("La sucesión invertida es:");
+                for (int i = 0; i < Sucesión.Length; i++)
+                {
+                    Console.WriteLine(Sucesión[i]);
+                }
+                Console.WriteLine("¿Desea ingresar otra sucesión?\n1: Si\n2: No");
+                opc = char.Parse(Console.ReadLine());
+            } while (opc == '1');
+            Console.ReadKey();
+        }
+    }
+}
