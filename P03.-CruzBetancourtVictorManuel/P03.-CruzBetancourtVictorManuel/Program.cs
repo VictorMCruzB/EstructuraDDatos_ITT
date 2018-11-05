bdaf11118089963ee5190544ee47c211af9aaf60
VictorMCruzB <43403238+VictorMCruzB@users.noreply.github.com>
2018-11-05 03:06:57 -0800

    Prácticas Evaluativas U3

---
+++ P03.-CruzBetancourtVictorManuel/P03.-CruzBetancourtVictorManuel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E03._CruzBetancourtVictorManuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Números = new int[15];
+
+            Console.WriteLine("Agrega 15 números");
+
+            for (int i = 0; i < Números.Length; i++)
+            {
+                Números[i] = int.Parse(Console.ReadLine());
+            }
+
+            int suma = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                suma += Números[i];
+            }
+            Console.WriteLine("La suma de los primeros 4 números es {0}", suma);
+
+            int suma1 = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                suma1 += Números[i];
+            }
+            Console.WriteLine("La suma de los primeros 9 números es {0}", suma1);
+        }
+    }
+}
