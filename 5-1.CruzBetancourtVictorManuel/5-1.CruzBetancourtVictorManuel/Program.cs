cf31f46b5b50ea40cd77cd8d37c34112b0862f7a
VictorMCruzB <43403238+VictorMCruzB@users.noreply.github.com>
2018-11-21 23:53:40 -0800

    Unidad 5

---
+++ 5-1.CruzBetancourtVictorManuel/5-1.CruzBetancourtVictorManuel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;  // using System;
+using System.Collections.Generic;  // using System.Collections.Generic;
+using System.Linq;  // using System.Linq;
+using System.Text;  // using System.Text;
+using System.Threading.Tasks;  // using System.Threading.Tasks;
+// Espacio entre código;
+namespace _5_1.CruzBetancourtVictorManuel  // Nombre de la Práctica;
+{  // Se abre llave;
+    class Program  // Clase Program;
+    {  // Se abre llave;
+        static void Main(string[] args)  // Main;
+        {  // Se abre llave;
+            BubbleSort Obj = new BubbleSort();  // Se crea un Objeto de la clase BubbleSort que lleva por nombre Obj;
+            Obj.Imprimir();  // Se manda a llamar al método Imprimir de la clase BubbleSort con ayuda del objeto Obj;
+        }  // Se cierra llave;
+    }  // Se cierra llave;
+}  // Se cierra llave;
