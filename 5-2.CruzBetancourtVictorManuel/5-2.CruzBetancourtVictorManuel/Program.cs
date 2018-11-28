3c61d53d1957f7ef246ac648ece4d7c07eb7ee3b
VictorMCruzB <43403238+VictorMCruzB@users.noreply.github.com>
2018-11-28 04:08:06 -0800

    Unidad 5

---
+++ 5-2.CruzBetancourtVictorManuel/5-2.CruzBetancourtVictorManuel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;  // using System;
+using System.Collections.Generic;  // using System.Collections.Generic;
+using System.Linq;  // using System.Linq;
+using System.Text;  // using System.Text;
+using System.Threading.Tasks;  // using System.Threading.Tasks;
+// Espacio entre código;
+namespace _5_2.CruzBetancourtVictorManuel  // Nombre de la práctica
+{  // Se abre la llave
+    class Program  // Clase Program
+    {  // Se abre la llave
+        static void Main(string[] args)  // Clase Main
+        { // Se abre la llave
+            Console.WriteLine("Metodo de ordenamiento QuickSort");  // Imprime el mensaje indicado en la consola y se genera un salto de línea;
+            Console.WriteLine("----------------------------------------------------------------------");  // Imprime en la mensaje indicado en la consola y se genera un salto de línea;
+            Ordenamiento Orde = new Ordenamiento();  // Se declara un objeto de la clase Ordenamiento, el cual lleva por nombre Orde;
+        }  // Se cierra la llave 
+    }  // Se cierra la llave
+}  // Se cierra la llave
