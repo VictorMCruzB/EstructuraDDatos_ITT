0825ef243580a51f9cfbed1ec82cb4c2a74894a1
VictorMCruzB <43403238+VictorMCruzB@users.noreply.github.com>
2018-12-10 19:51:39 -0800

    Unidad 6

---
+++ 6-2.CruzBetancourtVictorManuel/6-2.CruzBetancourtVictorManuel/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;  // using System;
+using System.Collections.Generic;  // using System.Collections.Generic;
+using System.Linq;  // using System.Linq;
+using System.Text;  // using System.Text;
+using System.Threading.Tasks;  // using System.Threading.Tasks;
+// Espacio entre código;
+namespace _6_2.CruzBetancourtVictorManuel  // Nombre de la práctica;
+{  // Se abre la llave;
+    class Program  // Clase Program;
+    {  // Se abre la llave;
+        static void Main(string[] args)  // Clase Main;
+        {  // Se abre la llave;
+            new Menú().MenúBúsqueda();  // Se manda a llamar el método MenúBúsqueda de la clase Menú;
+        }  // Se cierra la llave;
+    }  // Se cierra la llave;
+}  // Se cierra la llave;
